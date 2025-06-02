f5c34e5cf08d5523448f8c512779f6c8ba4075bc
Mat Bishop <mat@emgeebee.com>
2025-06-02 21:23:06 +0100

    pipeline and auth

--- auth/auth.js
+++ auth/auth.js
@@ -1,20 +1,50 @@
 'use strict';
 
-console.log('b');
-var verifier = require('google-id-token-verifier');
+// var verifier = require('google-id-token-verifier');
 
-module.exports.auth = function(event, context, callback) {
-	console.log('a', event, context);
-	// ID token from client.
-	var IdToken = 'XYZ123';
+// app's client IDs to check with audience in ID Token.
+var CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+const {OAuth2Client} = require('google-auth-library');
+const client = new OAuth2Client(CLIENT_ID);
 
-	// app's client IDs to check with audience in ID Token.
-	var clientId = '195751140228-9tkaoajmqv2ghuh0p1gs0a974aufffuo.apps.googleusercontent.com';
+async function verify(token) {
+  const ticket = await client.verifyIdToken({
+      idToken: token,
+      audience: CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
+      // Or, if multiple clients access the backend:
+      //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+  });
+  const payload = ticket.getPayload();
+  return payload['sub'];
+}
 
-	verifier.verify(IdToken, clientId, function (err, tokenInfo) {
-		if (!err) {
-			// use tokenInfo in here.
-			console.log(tokenInfo);
-		}
-	};
+
+// A function to generate a response from Authorizer to API Gateway.
+function generate_policy(principal_id, effect, resource, uid) {
+  return {
+    principalId: principal_id,
+    policyDocument: {
+      Version: '2012-10-17',
+      Statement: [{
+        Action: 'execute-api:Invoke',
+        Effect: effect,
+        // Resource: resource
+        Resource: '*'
+      }]
+    },
+    context: {
+        uid
+    }
+  };
+}
+
+module.exports.authorizer = (event, context, callback) => {
+    var token = event.authorizationToken ? event.authorizationToken.replace('Bearer ', '') : '';
+    verify(token).then(uid => {
+        console.log('eee', event.methodArn);
+        callback(null, generate_policy(1, 'Allow', event.methodArn, uid));
+    }).catch(err => {
+        console.error(err);
+        callback('Denied');
+    });
 };
--- holidays/get.js
+++ holidays/get.js
@@ -5,10 +5,11 @@ const AWS = require('aws-sdk'); // eslint-disable-line import/no-extraneous-depe
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
 module.exports.get = (event, context, callback) => {
+    console.log(event);
   const params = {
-    TableName: process.env.DYNAMODB_TABLE,
+    TableName: `${process.env.DYNAMODB_TABLE}-${process.env.STAGE}`,
     Key: {
-      id: event.pathParameters.id,
+      id: event.requestContext.authorizer.uid,
     },
   };
 
@@ -29,6 +30,10 @@ module.exports.get = (event, context, callback) => {
     const response = {
       statusCode: 200,
       body: JSON.stringify(result.Item),
+      headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Credentials': true,
+      },
     };
     callback(null, response);
   });
